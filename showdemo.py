bfe2ef9dd24ff6ae0dcad1f944b88e1d48a8aa4a
JefferyCYH <siwangzhishen30@qq.com>
2020-11-23 18:25:34 +0800

    Update showdemo.py

--- showdemo.py
+++ showdemo.py
@@ -17,57 +17,42 @@ import pdb
 
 class PETViewer(QWidget):
 
-    def __init__(self, pet_file, mri_file, wm_file):
+    def __init__(self):
         super().__init__()
-
-        pet_image = sitk.ReadImage(pet_file)
-        mri_image = sitk.ReadImage(mri_file)
-        wm_image = sitk.ReadImage(wm_file)
-
-        self.pet_arr = sitk.GetArrayFromImage(pet_image)
-        self.mri_arr = sitk.GetArrayFromImage(mri_image)
-        self.wm_arr = sitk.GetArrayFromImage(wm_image)
-
-        # rm_wm
-        # remove_ixs=np.where(self.wm_arr>0.1*np.max(self.wm_arr))
-        self.rm_arr = sitk.GetArrayFromImage(wm_image)
-        # self.rm_arr[remove_ixs] = 0
-
-        print("self.pet_arr.shape", self.pet_arr.shape)
-        print("self.mri_arr.shape", self.mri_arr.shape)
-        print("self.wm_arr.shape", self.wm_arr.shape)
-        print("self.rm_arr.shape", self.rm_arr.shape)
-
-        self.initUI()
-
-    def initUI(self):
         self.setWindowTitle("Viewer-demo")
-
-        sagittal_slice_max = self.pet_arr.shape[0]
-        coronal_slice_max = self.pet_arr.shape[1]
-        transverse_slice_max = self.pet_arr.shape[2]
-
-        # Median sagittal section 矢状位
-        self.sagittal = SubLayout("sagittal", sagittal_slice_max,coronal_slice_max, self.pet_arr, self.mri_arr, self.wm_arr, self.rm_arr)
-        # Coronal section 冠状位
-        self.coronal_layout = SubLayout("coronal", coronal_slice_max,sagittal_slice_max, self.pet_arr, self.mri_arr, self.wm_arr,
-                                        self.rm_arr)
-        # Transverse section 横断位
-        self.transverse_layout = SubLayout("transverse", transverse_slice_max, sagittal_slice_max,self.pet_arr, self.mri_arr, self.wm_arr,
-                                           self.rm_arr)
+        self.sagittal = SubLayout("sagittal")
 
         layout = QVBoxLayout()
         layout.addLayout(self.sagittal.layout)
-        # layout.addLayout(self.coronal_layout.layout)
-        # layout.addLayout(self.transverse_layout.layout)
         self.setLayout(layout)
+    
+    def change_image(self, image):
+        print("change_image_showdemo")
+        pet_image = sitk.ReadImage(image)
+
+        pet_arr = sitk.GetArrayFromImage(pet_image)
+
+        sagittal_slice_max = pet_arr.shape[0]
+        coronal_slice_max = pet_arr.shape[1]
+
+        self.sagittal.pet_arr=pet_arr
+        self.sagittal.mri_arr=pet_arr
+        self.sagittal.wm_arr=pet_arr
+        self.sagittal.rm_arr=pet_arr
+
+        self.sagittal.scrollbar_setmaximum(self.sagittal.scrollbar_time, sagittal_slice_max)
+        self.sagittal.scrollbar_setmaximum(self.sagittal.scrollbar_slice, coronal_slice_max)
+        return
 
 
 class SubLayout(PyQt5.QtWidgets.QVBoxLayout):
-    def __init__(self, tag, scrollbar_max,scrollbar_max2, pet_arr, mri_arr, wm_arr, rm_arr):
+    def __init__(self, tag):
         super().__init__()
 
-        self.pet_arr, self.mri_arr, self.wm_arr, self.rm_arr = pet_arr, mri_arr, wm_arr, rm_arr
+        self.pet_arr = None
+        self.mri_arr = None
+        self.wm_arr = None
+        self.rm_arr = None
         self.axis1_value = 0
         self.axis2_value = 0
         layout1 = QHBoxLayout()
@@ -81,18 +66,14 @@ class SubLayout(PyQt5.QtWidgets.QVBoxLayout):
         layout1.addWidget(self.plotCanvas4)
 
         layout2 = QHBoxLayout()
-        self.scrollbar = QScrollBar(Qt.Horizontal)
-        self.scrollbar.setMaximum(scrollbar_max)
-        self.scrollbar.sliderMoved.connect(lambda: self.sliderMoved("scrollbar_{}".format(tag)))
-        # self.value=self.scrollbar.sliderMoved.connect(lambda:self.sliderMoved("scrollbar_{}".format(tag)))
-        # print("transferred value", self.value)
-        layout2.addWidget(self.scrollbar)
+        self.scrollbar_time = QScrollBar(Qt.Horizontal)
+        self.scrollbar_time.sliderMoved.connect(lambda: self.sliderMoved("scrollbar_{}".format(tag)))
+        layout2.addWidget(self.scrollbar_time)
 
         layout3 = QHBoxLayout()
-        self.scrollbar = QScrollBar(Qt.Horizontal)
-        self.scrollbar.setMaximum(scrollbar_max2)
-        self.scrollbar.sliderMoved.connect(lambda: self.sliderMoved2("scrollbar_{}".format(tag)))
-        layout3.addWidget(self.scrollbar)
+        self.scrollbar_slice = QScrollBar(Qt.Horizontal)
+        self.scrollbar_slice.sliderMoved.connect(lambda: self.sliderMoved2("scrollbar_{}".format(tag)))
+        layout3.addWidget(self.scrollbar_slice)
 
         self.layout = QVBoxLayout()
         self.layout.addLayout(layout1)
@@ -101,21 +82,23 @@ class SubLayout(PyQt5.QtWidgets.QVBoxLayout):
 
     def sliderMoved(self, scrollbarName):
         self.axis1_value = self.sender().value()
-        print('time',self.axis1_value)
-
-        self.plotCanvas1.plot(scrollbarName, self.pet_arr, self.axis1_value, self.axis2_value, cmap='jet')
-        self.plotCanvas2.plot(scrollbarName, self.mri_arr, self.axis1_value, self.axis2_value, cmap='gray')
-        self.plotCanvas3.plot(scrollbarName, self.wm_arr, self.axis1_value, self.axis2_value, cmap='gray')
-        self.plotCanvas4.plot(scrollbarName, self.rm_arr, self.axis1_value, self.axis2_value, cmap='jet')
+        if not self.pet_arr is None:
+            self.plotCanvas1.plot(scrollbarName, self.pet_arr, self.axis1_value, self.axis2_value, cmap='jet')
+            self.plotCanvas2.plot(scrollbarName, self.mri_arr, self.axis1_value, self.axis2_value, cmap='gray')
+            self.plotCanvas3.plot(scrollbarName, self.wm_arr, self.axis1_value, self.axis2_value, cmap='gray')
+            self.plotCanvas4.plot(scrollbarName, self.rm_arr, self.axis1_value, self.axis2_value, cmap='jet')
 
     def sliderMoved2(self, scrollbarName):
         self.axis2_value = self.sender().value()
-        print('slice',self.axis2_value)
+        if not self.pet_arr is None:
+            self.plotCanvas1.plot(scrollbarName, self.pet_arr, self.axis1_value, self.axis2_value, cmap='jet')
+            self.plotCanvas2.plot(scrollbarName, self.mri_arr, self.axis1_value, self.axis2_value, cmap='gray')
+            self.plotCanvas3.plot(scrollbarName, self.wm_arr, self.axis1_value, self.axis2_value, cmap='gray')
+            self.plotCanvas4.plot(scrollbarName, self.rm_arr, self.axis1_value, self.axis2_value, cmap='jet')
 
-        self.plotCanvas1.plot(scrollbarName, self.pet_arr, self.axis1_value, self.axis2_value, cmap='jet')
-        self.plotCanvas2.plot(scrollbarName, self.mri_arr, self.axis1_value, self.axis2_value, cmap='gray')
-        self.plotCanvas3.plot(scrollbarName, self.wm_arr, self.axis1_value, self.axis2_value, cmap='gray')
-        self.plotCanvas4.plot(scrollbarName, self.rm_arr, self.axis1_value, self.axis2_value, cmap='jet')
+    def scrollbar_setmaximum(self, scrollbar, maximum):
+        scrollbar.setMaximum(maximum)
+
 
 
 class PlotCanvas(FigureCanvas):
@@ -152,9 +135,9 @@ class PlotCanvas(FigureCanvas):
 
 
 if __name__ == '__main__':
-    pet_file = 'D:/4dregression/preprocess/4D/patient001_4d_ed_es.nii'
-    mri_file = 'D:/4dregression/preprocess/4D/patient001_4d_ed_es.nii'
-    wm_file = 'D:/4dregression/preprocess/4D/patient001_4d_ed_es.nii'
+    pet_file = 'patient001_4d_ed_es.nii'
+    mri_file = 'patient001_4d_ed_es.nii'
+    wm_file = 'patient001_4d_ed_es.nii'
     app = QApplication(sys.argv)
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     example = PETViewer(pet_file, mri_file, wm_file)
