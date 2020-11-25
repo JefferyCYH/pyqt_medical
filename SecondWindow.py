a27493f57eb72c8f60e65224bad49a39c7250003
CHANGYU9 <1075854347@qq.com>
2020-11-25 19:58:21 +0800

    Add files via upload

--- SecondWindow.py
+++ SecondWindow.py
@@ -10,13 +10,12 @@ from custom.treeView import FileSystemTreeView
 from custom.listWidgets import FuncListWidget, UsedListWidget
 from custom.graphicsView import GraphicsView
 from imageshow.LGEshow import LGEView
-from imageshow.showdemo import PETViewer
+from imageshow.PETVshow import PETViewer
 
 
 class MyApp(QMainWindow):
     def __init__(self):
         super(MyApp, self).__init__()
-        self.datatype = "png"
         self.tool_bar = self.addToolBar('工具栏')
         self.action_right_rotate = QAction(QIcon("icons/右旋转.png"), "向右旋转90", self)
         self.action_left_rotate = QAction(QIcon("icons/左旋转.png"), "向左旋转90°", self)
@@ -105,7 +104,7 @@ class MyApp(QMainWindow):
 class MyApp_LGE(QMainWindow):
     def __init__(self):
         super(MyApp_LGE, self).__init__()
-        self.datatype = "lge"
+        print('创建APP_LGE')
         self.tool_bar = self.addToolBar('工具栏')
         self.action_right_rotate = QAction(QIcon("icons/右旋转.png"), "向右旋转90", self)
         self.action_left_rotate = QAction(QIcon("icons/左旋转.png"), "向左旋转90°", self)
@@ -116,11 +115,15 @@ class MyApp_LGE(QMainWindow):
         self.useListWidget = UsedListWidget(self)
         self.funcListWidget = FuncListWidget(self)
         self.stackedWidget = StackedWidget(self)
+        print(1)
         self.fileSystemTreeView = FileSystemTreeView(self)
+        print(2)
         self.LGEView = LGEView()
 
         self.dock_file = QDockWidget(self)
+        print(3)
         self.dock_file.setWidget(self.fileSystemTreeView)
+        print(4)
         self.dock_file.setTitleBarWidget(QLabel('目录'))
         self.dock_file.setFeatures(QDockWidget.NoDockWidgetFeatures)
 
@@ -133,7 +136,7 @@ class MyApp_LGE(QMainWindow):
         self.dock_used.setWidget(self.useListWidget)
         self.dock_used.setTitleBarWidget(QLabel('已选操作'))
         self.dock_used.setFeatures(QDockWidget.NoDockWidgetFeatures)
-        # self.dock_used.setFeatures(QDockWidget.NoDockWidgetFeatures)
+        self.dock_used.setFeatures(QDockWidget.NoDockWidgetFeatures)
 
         self.dock_attr = QDockWidget(self)
         self.dock_attr.setWidget(self.stackedWidget)
@@ -141,6 +144,7 @@ class MyApp_LGE(QMainWindow):
         self.dock_attr.setFeatures(QDockWidget.NoDockWidgetFeatures)
         self.dock_attr.close()
 
+        # self.setCentralWidget(self.graphicsView)
         self.setCentralWidget(self.LGEView)
         self.addDockWidget(Qt.LeftDockWidgetArea, self.dock_file)
         self.addDockWidget(Qt.TopDockWidgetArea, self.dock_func)
@@ -152,8 +156,10 @@ class MyApp_LGE(QMainWindow):
         self.src_img = None
         self.cur_img = None
 
+        print('创建APP_LGE完成')
 
     def update_image(self):
+        print('update the image')
         if self.src_img is None:
             return
         img = self.process_image()
@@ -162,6 +168,7 @@ class MyApp_LGE(QMainWindow):
         self.LGEView.update_image(img)
 
     def change_image(self, img):
+        print('change the image')
         # self.src_img = img
         # img = self.process_image()
         # self.cur_img = img
@@ -182,3 +189,91 @@ class MyApp_LGE(QMainWindow):
         # self.graphicsView.rotate(-90)
         self.LGEView.rotate(-90)
 
+
+class MyApp_PETV(QMainWindow):
+    def __init__(self):
+        super(MyApp_PETV, self).__init__()
+        print('创建APP_LGE')
+        self.tool_bar = self.addToolBar('工具栏')
+        self.action_right_rotate = QAction(QIcon("icons/右旋转.png"), "向右旋转90", self)
+        self.action_left_rotate = QAction(QIcon("icons/左旋转.png"), "向左旋转90°", self)
+        self.action_right_rotate.triggered.connect(self.right_rotate)
+        self.action_left_rotate.triggered.connect(self.left_rotate)
+        self.tool_bar.addActions((self.action_left_rotate, self.action_right_rotate))
+
+        self.useListWidget = UsedListWidget(self)
+        self.funcListWidget = FuncListWidget(self)
+        self.stackedWidget = StackedWidget(self)
+        print(1)
+        self.fileSystemTreeView = FileSystemTreeView(self)
+        print(2)
+        self.PETViewer = PETViewer()
+
+        self.dock_file = QDockWidget(self)
+        print(3)
+        self.dock_file.setWidget(self.fileSystemTreeView)
+        print(4)
+        self.dock_file.setTitleBarWidget(QLabel('目录'))
+        self.dock_file.setFeatures(QDockWidget.NoDockWidgetFeatures)
+
+        self.dock_func = QDockWidget(self)
+        self.dock_func.setWidget(self.funcListWidget)
+        self.dock_func.setTitleBarWidget(QLabel('图像操作'))
+        self.dock_func.setFeatures(QDockWidget.NoDockWidgetFeatures)
+
+        self.dock_used = QDockWidget(self)
+        self.dock_used.setWidget(self.useListWidget)
+        self.dock_used.setTitleBarWidget(QLabel('已选操作'))
+        self.dock_used.setFeatures(QDockWidget.NoDockWidgetFeatures)
+        self.dock_used.setFeatures(QDockWidget.NoDockWidgetFeatures)
+
+        self.dock_attr = QDockWidget(self)
+        self.dock_attr.setWidget(self.stackedWidget)
+        self.dock_attr.setTitleBarWidget(QLabel('属性'))
+        self.dock_attr.setFeatures(QDockWidget.NoDockWidgetFeatures)
+        self.dock_attr.close()
+
+        # self.setCentralWidget(self.graphicsView)
+        self.setCentralWidget(self.PETViewer)
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.dock_file)
+        self.addDockWidget(Qt.TopDockWidgetArea, self.dock_func)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dock_used)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dock_attr)
+
+        self.setWindowTitle('医学图像处理')
+        self.setWindowIcon(QIcon('icons/main.png'))
+        self.src_img = None
+        self.cur_img = None
+
+        print('创建APP_LGE完成')
+
+    def update_image(self):
+        print('update the image')
+        if self.src_img is None:
+            return
+        img = self.process_image()
+        self.cur_img = img
+        # self.graphicsView.update_image(img)
+        self.LGEView.update_image(img)
+
+    def change_image(self, img):
+        print('change the image')
+        # self.src_img = img
+        # img = self.process_image()
+        # self.cur_img = img
+        # self.graphicsView.change_image(img)
+        self.PETViewer.change_image(img)
+
+    def process_image(self):
+        img = self.src_img.copy()
+        for i in range(self.useListWidget.count()):
+            img = self.useListWidget.item(i)(img)
+        return img
+
+    def right_rotate(self):
+        # self.graphicsView.rotate(90)
+        self.LGEView.rotate(90)
+
+    def left_rotate(self):
+        # self.graphicsView.rotate(-90)
+        self.LGEView.rotate(-90)
