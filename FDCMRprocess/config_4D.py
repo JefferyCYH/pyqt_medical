ea8a7e4dc3706a74342988fd184b3c70a4fb4150
JefferyCYH <siwangzhishen30@qq.com>
2021-08-07 11:22:38 +0800

    Create config_4D.py
    新增4DCMR处理集合

---
+++ FDCMRprocess/config_4D.py
@@ -0,0 +1,22 @@
+from FDCMRprocess.tableWidget_4D import *
+from FDCMRprocess.listWidgetItems_4D import *
+
+
+# Implemented functions
+items = [
+    LabelItem,
+    NormItem,
+    LightItem,
+    ROIItem,
+    RegItem,
+    SegItem,
+]
+
+tables = [
+    LabelTableWidget,
+    NormTableWidget,
+    ROITableWidget,
+    RegTableWidget,
+    SegTableWidget,
+]
+
