3996971e459a1277c20e1600a55e56c019cd8648
JefferyCYH <siwangzhishen30@qq.com>
2021-08-07 11:24:24 +0800

    Create config_LGE.py

---
+++ LGEprocess/config_LGE.py
@@ -0,0 +1,22 @@
+from LGEprocess.tableWidget_LGE import *
+from LGEprocess.listWidgetItems_LGE import *
+
+
+# Implemented functions
+items = [
+    LabelItem,
+    NormItem,
+    LightItem,
+    ROIItem,
+    RegItem,
+    SegItem,
+]
+
+tables = [
+    LabelTableWidget,
+    NormTableWidget,
+    ROITableWidget,
+    RegTableWidget,
+    SegTableWidget,
+]
+
