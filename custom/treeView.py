c56bea65c1e6c997b83ba362f4ecb693b868124c
JefferyCYH <siwangzhishen30@qq.com>
2021-08-07 11:24:02 +0800

    Update treeView.py

--- custom/treeView.py
+++ custom/treeView.py
@@ -1,8 +1,15 @@
 import cv2
 import numpy as np
-
+import nibabel as nib
+import SimpleITK as sitk
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+
+def load_nii(path):
+    image = nib.load(path)
+    affine = image.affine
+    image = np.asarray(image.dataobj)
+    return image, affine
 
 
 class FileSystemTreeView(QTreeView, QDockWidget):
@@ -32,10 +39,16 @@ class FileSystemTreeView(QTreeView, QDockWidget):
             src_img = cv2.imdecode(np.fromfile(file_name, dtype=np.uint8), -1)
             self.mainwindow.change_image(src_img)
         elif file_name.endswith('.nii.gz') and self.mainwindow.datatype == "lge":
-            src_img = file_name
+            src_img_name = file_name
+            src_img, affine = load_nii(file_name)
+            print(src_img.shape)
+            # src_img = sitk.ReadImage(file_name)
+            # src_img=sitk.GetArrayFromImage(src_img)
             self.mainwindow.change_image(src_img)
         elif file_name.endswith('.nii.gz') and self.mainwindow.datatype == "petv":
-            src_img = file_name
+            src_img_name = file_name
+            src_img, affine = load_nii(file_name)
+            src_img=np.transpose(src_img,(3,0,1,2))
             self.mainwindow.change_image(src_img)
         elif file_name.endswith('.raw') and self.mainwindow.datatype == "raw":
             src_img = file_name
